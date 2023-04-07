b5f104221701cdf8c0c94dbe3117f9777030b31b
ernest <ernestas.du@gmail.com>
2023-04-07 09:34:12 +0100

    remove global css, used tailwind instead

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Hero(): JSX.Element {
     return (
-        <section className="bg-gray">
+        <section className="bg-gray-50">
             <div className="mx-auto max-w-5xl px-8 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
                 <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-16">
                     <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
@@ -18,16 +18,16 @@ function Hero(): JSX.Element {
                     </div>
 
                     <div className="lg:py-24">
-                        <h2 className="text-3xl font-bold sm:text-4xl color-black">Success Starts with the Glow Gang Community!</h2>
+                        <h2 className="text-3xl font-bold sm:text-4xl text-black ">Success Starts with the Glow Gang Community!</h2>
 
-                        <p className="mt-4 color-gray">
+                        <p className="mt-4 text-gray-400">
                             Glow Gang: a dynamic community empowering the leaders of tomorrow through connection, inspiration, and a shared passion for achievement.
                         </p>
 
                         <div className="mt-8">
                             <Link
                                 href="/sign-up"
-                                className="inline-block rounded bg-red px-12 py-3 text-sm font-medium text-white transition hover:color-red focus:outline-none focus:ring focus:ring-yellow-400"
+                                className="inline-block rounded bg-red-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-red-500 focus:outline-none focus:ring focus:ring-yellow-400"
                             >
                                 Join the Glow Gang community!
                             </Link>
