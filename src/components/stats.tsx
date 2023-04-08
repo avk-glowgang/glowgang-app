cc1fe23bcf2afa06c57863924581920f7d00c72d
reddiedev <105657266+reddiedev@users.noreply.github.com>
2023-04-09 00:27:44 +0800

    updated readme

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface StatCardProps {
     label: string;
@@ -7,22 +7,18 @@ interface StatCardProps {
 
 const StatCard: React.FC<StatCardProps> = ({ label, value }) => (
     <div className="flex flex-col rounded border border-gray-100 px-4 py-8 text-center">
-        <dt className="order-last text-md font-medium text-gray-500">{label}</dt>
+        <dt className="text-md order-last font-medium text-gray-500">{label}</dt>
         <dd className="text-3xl font-extrabold text-blue-600 md:text-4xl">{value}</dd>
     </div>
 );
 
 const ECommerceStats: React.FC = () => (
     <section className="bg-white">
-        <div className="mx-auto max-w-5xl px-8 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+        <div className="mx-auto max-w-5xl px-8 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
             <div className="mx-auto max-w-3xl text-center">
-                <h2 className="text-2xl font-bold sm:text-3xl text-black ">
-                    We are gowing fast! 🚀
-                </h2>
+                <h2 className="text-2xl font-bold text-black sm:text-3xl ">We are gowing fast! 🚀</h2>
 
-                <p className="mt-4 text-gray-400">
-                    Now is the best join to join our community! We are growing fast and we want you to be a part of it. 
-                </p>
+                <p className="mt-4 text-gray-400">Now is the best join to join our community! We are growing fast and we want you to be a part of it.</p>
             </div>
 
             <div className="mt-8 sm:mt-12">
