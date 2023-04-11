fb3f1e76a8badfd2d0182e1a2bea25b9a113c5bb
ernest <128949580+avk-glowgang@users.noreply.github.com>
2023-04-11 13:53:59 +0100

    move podcasts to db

---
+++ src/components/breadcrumbs.tsx
@@ -0,0 +1,49 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import React from "react";
+
+interface BreadcrumbItem {
+    label: string;
+    href: string;
+}
+
+interface BreadcrumbsProps {
+    items: BreadcrumbItem[];
+}
+
+const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ items }) => {
+    const router = useRouter();
+
+    return (
+        <nav aria-label="Breadcrumb" className="container mx-auto max-w-5xl px-8 my-5">
+            <ol role="list" className="flex items-center gap-1 text-sm text-gray-600">
+                {items.map((item, index) => (
+                    <React.Fragment key={item.href}>
+                        {index > 0 && (
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </li>
+                        )}
+                        <li>
+                            {item.href === router.pathname ? (
+                                <span>{item.label}</span>
+                            ) : (
+                                <Link href={item.href}>
+                                    <p className="block transition text-blue-600 hover:text-gray-700">{item.label}</p>
+                                </Link>
+                            )}
+                        </li>
+                    </React.Fragment>
+                ))}
+            </ol>
+        </nav>
+    );
+};
+
+export default Breadcrumbs;
--- src/pages/pro/index.tsx
+++ src/pages/pro/index.tsx
@@ -2,8 +2,22 @@ import { type NextPage, GetServerSidePropsContext } from "next";
 import Head from "next/head";
 import Navbar from "@components/navbar";
 import Header from "@components/header";
+import Breadcrumbs from "@components/breadcrumbs";
 
 const ProInfoPage: NextPage = () => {
+
+    // Breadcumbs
+    const breadcrumbs = [
+        {
+            label: "Dashboard",
+            href: "/pro/portal",
+        },
+        {
+            label: "Become a PRO Member",
+            href: "/pro",
+        },
+    ];
+
     return (
         <>
             <Head>
@@ -16,36 +30,9 @@ const ProInfoPage: NextPage = () => {
             </Head>
             <Navbar />
             <Header />
-            {/* Info and Payment Information for Membership */}
+            <Breadcrumbs items={breadcrumbs} />
+
             <section className="bg-white">
-                <nav aria-label="Breadcrumb" className="container mx-auto max-w-5xl px-8 my-5">
-                    <ol role="list" className="flex items-center gap-1 text-sm text-gray-600">
-
-                        <li>
-                            <a href="/pro/portal" className="block transition text-blue-600 hover:text-gray-700"> Dashboard </a>
-                        </li>
-
-                        <li>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-4 w-4"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                        </li>
-                        <li>
-                            <p className="block transition hover:text-gray-700"> PRO Membership </p>
-                        </li>
-                    </ol>
-                </nav>
-
-
                 <div className="container mx-auto max-w-5xl px-8 pt-10 pb-10 mb-20">
                     <div className="text-center lg:mb-12">
                         <h2 className="mb-4 text-4xl font-extrabold text-gray-900">Become a <span className="whitespace-nowrap  rounded-full bg-black px-5 py-0.5 text-white">
