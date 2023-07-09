74c110d7b42feb30cd18b648ccca75f558cdb596
Rahmat Kurniawan <rahmatkurniawan095@gmail.com>
2023-07-09 12:25:16 +0700

    feat: add profile page and modified navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,56 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { signOut, useSession } from "next-auth/react";
+import { Menu, Transition } from '@headlessui/react'
+
+const MyMenu: React.FC<{ username: string }> = ({ username }) => {
+    return (
+        <Menu as="div" className="relative inline-block text-left">
+            <Menu.Button className="text-gray-200 text-sm inline-flex gap-2 items-center px-4 py-2 font-semibold rounded-md hover:bg-gray-800 hover:bg-opacity-50 transition ease-in-out duration-150">
+                {username}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+            </Menu.Button>
+            
+            <Transition
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0"
+            >
+                <Menu.Items className="absolute text-sm right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1">
+                        <Menu.Item>
+                            <Link href="/profile">
+                                <button className={`text-gray-900 group inline-flex gap-1 w-full items-center hover:bg-gray-100 rounded-md px-2 py-2 transition ease-in-out duration-150`}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-4 h-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                    </svg>
+                                    Profile
+                                </button>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <button onClick={() => void signOut()} 
+                                className={`text-red-400 font-semibold group inline-flex gap-1 hover:bg-red-100 w-full items-center rounded-md px-2 py-2 transition ease-in-out duration-150`}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="w-4 h-4">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                                </svg>
+
+                                Sign Out 
+                            </button>
+                        </Menu.Item>
+                    </div>
+                </Menu.Items>
+                
+            </Transition>
+        </Menu>
+    )
+}
+
 
 function UserButton(): JSX.Element {
     return (
@@ -30,7 +80,6 @@ function Navbar(): JSX.Element {
                             <Image src="/logo-PNG-05.png" width={150} height={150} alt="Glow Gang Logo" />
                         </Link>
                     </div>
-
                     {/* display for unauthenticated users only when they are not in the sign in page */}
                     {session.status !== "authenticated" && !isSignIn && (
                         <div>
@@ -41,7 +90,10 @@ function Navbar(): JSX.Element {
                     )}
 
                     {/* display for authenticated users */}
-                    {session.status == "authenticated" && <UserButton></UserButton>}
+                    {session.status == "authenticated" && <div className="flex gap-2 items-center">
+                        <MyMenu username={session.data.user.name}/>
+                        {/* <UserButton></UserButton> */}
+                    </div>}
                 </nav>
             </div>
         </div>
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,8 +1,36 @@
 import { type NextPage, type GetServerSidePropsContext } from "next";
 import Head from "next/head";
 import Navbar from "@components/navbar";
+import { type Session } from "next-auth/core/types";
+import type { User } from "@prisma/client";
+import Image from "next/image";
 
-const Profile: NextPage = () => {
+type LabelProps = {
+    placeholder: string,
+    value: string | null | React.ReactNode
+}
+
+const Label: React.FC<LabelProps> = ({ placeholder, value }) => {
+    return <div className="flex items-center text-sm">
+        <div className="w-[300px] text-gray-400">{placeholder}</div>
+        <div className="text-semibold text-gray-800">{value}</div>
+    </div>
+}
+
+const Badge: React.FC<{isPro: boolean}> = ({ isPro }) => {
+    if (isPro)
+        return <div className="text-xs font-semibold text-white bg-gray-900 rounded px-2 py-1">PRO</div>
+    
+    return <div className="text-xs font-semibold text-purple-700 bg-purple-100 rounded px-2 py-1">Member</div>
+}
+
+
+type Props = {
+    sessions: Session,
+    user: User
+}
+
+const Profile: NextPage<Props> = ({ user }) => {
     return (
         <>
             <Head>
@@ -14,6 +42,43 @@ const Profile: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Navbar />
+
+            <main className="mx-auto max-w-7xl mt-8">
+                <h1 className="font-semibold text-2xl mb-4 text-gray-800">Profile</h1>
+
+                <div className="flex gap-[2rem] border-b-[1px] pb-4">
+                    <div className="bg-gray-300 rounded-lg w-[150px] h-[150px]"></div>
+                    <div className="flex flex-col gap-2">
+                        <Label placeholder="Email" value={user.email}/>
+                        <Label placeholder="Name" value={user.name}/>
+                        <Label placeholder="Status" value={<Badge isPro={user.isPro}/>}/>
+
+                        <div className="mt-4">
+                        {user.isPro ? 
+                            <button className="text-sm font-semibold text-gray-800 inline-flex gap-1 items-center border rounded-lg border-gray-800 px-4 py-2 w-fit">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
+                                </svg>
+                                <span>Manage Billing</span>
+                            </button>
+                        : 
+                            <button
+                                className="block rounded-lg bg-black px-4 py-2 text-sm font-medium text-white transition hover:bg-gray-900 focus:outline-none focus:ring"
+                                type="button">
+                                <Link href="/pro">
+                                    <p>Become a PRO member</p>
+                                </Link>
+                            </button>
+                        }
+                            
+                        </div>
+                    </div>
+                    {/* {user.image 
+                        ? <Image src={user.image} alt="profile-user" width={200} height={200}/>
+                        : <>No Image</>
+                    } */}
+                </div>
+            </main>
         </>
     );
 };
@@ -23,6 +88,9 @@ export default Profile;
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@server/auth";
 import { env } from "src/env.mjs";
+import { prisma } from "@server/db";
+import React from "react";
+import Link from "next/link";
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
     // TODO: remove when page is launched
@@ -36,6 +104,17 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     }
 
     const session = await getServerSession(context.req, context.res, authOptions);
+    const user = await prisma.user.findFirst({ 
+        where: { id: session?.user.id }
+        // include: { 
+        //     accounts: {
+        //         select: {
+        //             provider: true,
+        //             type: true
+        //         }
+        //     } 
+        // }
+    });
 
     if (!session) {
         return {
@@ -46,9 +125,15 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
         };
     }
 
+
     return {
         props: {
-            session
+            session,
+            user: { 
+                ...user, 
+                createdAt: user?.createdAt.toISOString(),
+                updatedAt: user?.updatedAt.toISOString(),
+            }
         }
     };
 }
